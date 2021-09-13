3db52d1e5fa97c1f8af1d30d629476cdb43bf89f
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2021-09-13 13:56:46 +0300

    hw3 done

--- src/main/java/ru/sectorsj/interview_preparation/hw1/third_task/shapes/option2/part1/polygon/Polygon.java
+++
@@ -1,6 +0,0 @@
-package ru.sectorsj.interview_preparation.hw1.third_task.shapes.option2.part1.polygon;
-
-import ru.sectorsj.interview_preparation.hw1.third_task.shapes.option2.part1.Shape1;
-
-public interface Polygon extends Shape1 {
-}
