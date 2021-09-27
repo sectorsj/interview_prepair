7331e6601eec42d4d16d4a86f09fc6325a891abe
Tsotsonava Evgeniy <graund_shadow@mail.ru>
2021-09-27 16:33:35 +0300

    update hw6

--- src/main/java/ru/sectorsj/interview_preparation/hw6/ee/servlet/HelloWorld.java
+++ src/main/java/ru/sectorsj/interview_preparation/hw6/ee/servlet/HelloWorld.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class HelloWorld extends HttpServlet {
+
     private static Logger logger = LoggerFactory.getLogger(HelloWorld.class);
 
     @Override
